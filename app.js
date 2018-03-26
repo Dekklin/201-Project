1a2b65ad7838c9dddcffb752c382dd3791ea27cd
Katy Bowers <katyvine@gmail.com>
2018-03-26 13:00:09 -0700

    start psudo code for home page

---
+++ app.js
@@ -0,0 +1,13 @@
+'use strict'
+
+// attach event listner to login submit button
+
+// attach submit value to userName in array
+
+// store userName in local storage
+
+// check local storage if user has played before
+  // if (userName) then display 'welcome back ____'. 
+  // If wrong user, allow option to create new user
+
+// 
