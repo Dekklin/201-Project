9cc3ab4b578864d2f389c5584567a1ebe4381e06
ria agnes jose <joseagnesria@gmail.com>
2018-03-26 14:06:20 -0700

    created new branch

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-'use strict'
+'use strict';
 
 // attach event listner to login submit button
 
@@ -7,7 +7,7 @@
 // store userName in local storage
 
 // check local storage if user has played before
-  // if (userName) then display 'welcome back ____'. 
-  // If wrong user, allow option to create new user
+// if (userName) then display 'welcome back ____'.
+// If wrong user, allow option to create new user
 
-// 
+//
